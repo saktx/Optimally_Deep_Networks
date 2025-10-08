7602d815fa5200bb88f14423af2fee05e8ffec4f
Shaharyar <37941877+saktx@users.noreply.github.com>
2025-10-08 01:39:05 -0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+
+class AverageMeter:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.values = []
+        self.counter = 0
+
+    def append(self, val):
+        self.values.append(val)
+        self.counter += 1
+
+    @property
+    def val(self):
+        return self.values[-1]
+
+    @property
+    def avg(self):
+        return sum(self.values) / len(self.values)
+
+    @property
+    def last_avg(self):
+        if self.counter == 0:
+            return self.latest_avg
+        else:
+            self.latest_avg = sum(self.values[-self.counter:]) / self.counter
+            self.counter = 0
+            return self.latest_avg
+
+def requires_grad_(model:nn.Module, requires_grad:bool) -> None:
+    for param in model.parameters():
+        param.requires_grad_(requires_grad)
